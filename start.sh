ab673570fc83f89d52fa0cabd27130c7ade920f1
Vinay <78520329+vinay366@users.noreply.github.com>
2024-02-10 09:25:38 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/HarshalPurohitEdits/TheMovieProviderBot.git /TheMovieProviderBot
+  git clone https://github.com/vinay366/TheMovieProviderBot.git /TheMovieProviderBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /TheMovieProviderBot
